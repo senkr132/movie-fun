f67c9387e731f18741b5cba99c70931a03ef539d
senkr132 <senkr.132@gmail.com>
2020-03-15 16:19:46 -0700

    write in progress

---
+++ src/main/java/org/superbiz/moviefun/blobstore/FileStore.java
@@ -0,0 +1,22 @@
+package org.superbiz.moviefun.blobstore;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class FileStore implements BlobStore {
+
+    @Override
+    public void put(Blob blob) throws IOException {
+        // ...
+    }
+
+    @Override
+    public Optional<Blob> get(String name) throws IOException {
+        // ...
+    }
+
+    @Override
+    public void deleteAll() {
+        // ...
+    }
+}
